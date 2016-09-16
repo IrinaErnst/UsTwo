578cf6d045ba4ffb498e5e3125fe00f4499389fa
IrinaErnst <irinaernst11@gmail.com>
2016-09-16 19:56:49 -0400

    Initial commit

---
+++ UsTwo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UsTwo
+//
+//  Created by Irina Kalashnikova on 9/16/16.
+//  Copyright © 2016 Irina Ernst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myProfileImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
