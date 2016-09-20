9536d5e12920fbfecbaf06750178b3a7aab239ad
IrinaErnst <irinaernst11@gmail.com>
2016-09-20 09:03:53 -0400

    Add gitirnore and pod OHHTTPStubs

---
+++ Pods/Target Support Files/Pods-UsTwoUITests/Pods-UsTwoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UsTwoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UsTwoUITests
+@end
---
+++ UsTwo/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  UsTwo
+//
+//  Created by Irina Ernst on 9/20/16.
+//  Copyright © 2016 Irina Ernst. All rights reserved.
+//
+
+import Foundation
+
+let GITHUB_CLIENT_ID = "2eeb39a9ccdc3ff147bd"
+let GITHUB_CLIENT_SECRET = "f7ce3481c2e62d7b442d5d95beb17a2f6c8c163b"
+let GITHUB_ACCESS_TOKEN = "50f505d6e846775885e4655c6b1fcba58dbc70ab"
